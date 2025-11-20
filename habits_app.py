2d4092e9ebc718701ab277e59511f2eeee0d5390
arom-AI <titouanhorst@gmail.com>
2025-11-20 18:20:37 +0100

    Update habits_app.py

--- habits_app.py
+++ habits_app.py
@@ -240,15 +240,15 @@ h1, h2, h3, h4, h5, h6 {
 
 SHEET_ID = st.secrets["google_sheets"]["sheet_id"]
 
-# On utilise directement le fichier JSON de service account
-SERVICE_ACCOUNT_FILE = BASE_DIR / "habit-builder-streamlit-89aae6f136c1.json"
+if "gcp_service_account" in st.secrets:
+    gc = gspread.service_account_from_dict(st.secrets["gcp_service_account"])
+else:
+    # mode local éventuel
+    SERVICE_ACCOUNT_FILE = BASE_DIR / "habit-builder-streamlit-838d1b385e36.json"
+    gc = gspread.service_account(filename=str(SERVICE_ACCOUNT_FILE))
 
-gc = gspread.service_account(filename=str(SERVICE_ACCOUNT_FILE))
 sh = gc.open_by_key(SHEET_ID)
 
-ws_habits = sh.worksheet("habits")
-ws_checkins = sh.worksheet("checkins")
-ws_meta = sh.worksheet("meta")
 
 # ---------------------------------------------------------------------
 # LOAD DATA
